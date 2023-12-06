9e8e9b9b6fd47c76ccf3ee8bfc939f9b0631d5ff
NikolasVellnow <nikolasvellnow@gmail.com>
2023-12-06 16:26:20 +0100

    coding

--- summary_stats_cov.sh
+++ summary_stats_cov.sh
@@ -6,6 +6,8 @@ INPUT_FILE=$1
 NUM_SAMPLES=$2
 ((MAX_COL=$NUM_SAMPLES+2))
 
+
+samtools depth -H -@ $NUM_THREADS -f $SAMPLE_LIST 
 # calculating mean, standard deviation, and coefficient of variation for each sample (row=sample)
 zcat $INPUT_FILE | head -10 | awk '{n++; for(i=3;i<='"$MAX_COL"';i++) sum[i]+=$i;}END{for(i=3;i<='"$MAX_COL"';i++) print sum[i]/n,"\011",sum[i]/n}'
 
