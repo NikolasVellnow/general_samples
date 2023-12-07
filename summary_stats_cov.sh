437d36925abf5d093601940e8000f9187ec1bf00
NikolasVellnow <42894685+NikolasVellnow@users.noreply.github.com>
2023-12-07 12:53:34 +0100

    wrote awk command to calculate mean, sd and cv of coverage

--- summary_stats_cov.sh
+++ summary_stats_cov.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-INPUT_FILE=$1
+SAMPLE_LIST=$1
 
 # number of samples
 NUM_SAMPLES=$2
+#NUM_SAMPLES=3
 ((MAX_COL=$NUM_SAMPLES+2))
 
+NUM_THREADS=$3
 
-samtools depth -H -@ $NUM_THREADS -f $SAMPLE_LIST 
 # calculating mean, standard deviation, and coefficient of variation for each sample (row=sample)
-zcat $INPUT_FILE | head -10 | awk '{n++; for(i=3;i<='"$MAX_COL"';i++) sum[i]+=$i;}END{for(i=3;i<='"$MAX_COL"';i++) print sum[i]/n,"\011",sum[i]/n}'
 
-#zcat $INPUT_FILE | head -10 | awk '{n++; for(i=3;i<='"$MAX_COL"';i++) sum[i]+=$i;}END{for(i=3;i<='"$MAX_COL"';i++) print sum[i]/n,"\011",sum[i]/n}'
+samtools depth -@ $NUM_THREADS -f $SAMPLE_LIST | \
+awk '{for (i=3;i<='"$MAX_COL"';i++) sum[i]+=$i; \
+for (i=3;i<='"$MAX_COL"';i++) sum_sq[i]+=($i)^2}\
+END{for(i=3;i<='"$MAX_COL"';i++) print sum[i]/NR,"\011",\
+sqrt(1/NR *sum_sq[i] - (sum[i]/NR)^2),"\011",\
+(sqrt(1/NR *sum_sq[i] - (sum[i]/NR)^2))/((sum[i]+0.0001)/NR)}'
+
+#awk '{n++; for(i=3;i<='"$MAX_COL"';i++) sum_sq[i]+=($i)^2}END{for(i=3;i<='"$MAX_COL"';i++) print sum_sq[i]/n}'
+
+
 
 #zcat $INPUT_FILE | head -10 | awk '{n++; for(i=3;i<='"$MAX_COL"';i++) sum[i]+=$i;}END{for(i=3;i<='"$MAX_COL"';i++) print sum[i]/n,"\011",sum[i]/n}'
