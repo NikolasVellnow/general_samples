a548ed68ad40314bf328bb9be2180aff7f5b09ed
NikolasVellnow <42894685+NikolasVellnow@users.noreply.github.com>
2024-01-12 12:58:36 +0100

    updated plot to make SFS histogram

--- create_SFS_plot.R
+++ create_SFS_plot.R
@@ -3,13 +3,24 @@ setwd("~/sciebo/Postdoc TU Dortmund/general_samples")
 
 # function to calculate proportion of snps
 normalize <- function(x) x/sum(x)
-sfs <- scan("teutoburg_sfs_no_filter.sfs")
+sfs <- scan("teutoburg_sfs.sfs")
 # take only the first counts because it is a "folded" spectrum
 sfs <- sfs[2:11]
 sfs <- normalize(sfs)
+
+barplot(sfs,
+        names = seq(1:10),
+        main = "Teutoburg SFS",
+        xlab = "Allele frequency",
+        ylab = "Proportion of SNPs",
+        ylim = c(0.0, 0.4))
+grid(nx = NA,
+     ny = 8,
+     lty = 2, col = "gray", lwd = 1.5)
 barplot(sfs,
         names = seq(1:10),
         main = "Teutoburg SFS (no quality filter)",
         xlab = "Allele frequency",
         ylab = "Proportion of SNPs",
-        ylim = c(0.0, 0.4))
+        ylim = c(0.0, 0.4),
+        add = TRUE)
